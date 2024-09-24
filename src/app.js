1d226a7034ab00a90a25259c9a668817d8351b5a
Justin Berg <justin.berg1@verizon.com>
2024-09-24 13:42:09 -0500

    created get route

--- src/app.js
+++ src/app.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const app = express();
-const { Musician } = require("../models/index")
-const { db } = require("../db/connection")
+const { Musician } = require("../models/index");
+const { db } = require("../db/connection");
 
 const port = 3000;
 
-//TODO: Create a GET /musicians route to return all musicians 
+//TODO: Create a GET /musicians route to return all musicians
 
-
-
-
-
-
+app.get("/musicians", async (req, res) => {
+  try {
+    const musicians = await Musician.findAll();
+    res.json(musicians);
+  } catch (error) {
+    console.error("Error fetching musicians:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
 
 module.exports = app;
